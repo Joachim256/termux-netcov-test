6e08c711559d79eb983dbc7323bccbd6db71d39e
Joachim256 <jachymnet@seznam.cz>
2024-09-20 10:49:23 +0200

    Get location

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function check_dependencies {
+	if ! which termux-location > /dev/null 2>&1; then
+		echo "Termux tools aren't installed"
+		exit 1
+	fi
+	if ! which iperf3 > /dev/null 2>&1; then
+		echo "iperf3 isn't installed"
+		exit 1
+	fi
+}
+
+function run_test {
+	# get location
+	termux-location | \
+		jq -r '(.latitude | tostring) + "," + (.longitude | tostring)' \
+		> location.tmp
+	pid1=$!
+
+	# run iperf3 speed test
+	
+
+	# wait for both to complete
+	wait $pid1
+	#wait $pid2
+
+	# write to log
+	location=$(<location.tmp)
+}
+
+check_dependencies;
+
+while :
+do
+	run_test;
+	sleep 30
+done
